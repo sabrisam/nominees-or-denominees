4dd443b924b9e6fb9c5444513963de00e5d281f6
Sami Sabri <46608210+sabrisam@users.noreply.github.com>
2026-05-23 22:43:16 -0400

    auto: src/lib/supabase.ts @ 22:43:15

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,6 +2,11 @@ import { createClient, type SupabaseClient } from "@supabase/supabase-js";
 
 const isBrowser = typeof window !== "undefined";
 
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
 let browserClient: SupabaseClient | null = null;
 
 export function getSupabaseBrowserClient() {
