5c4943e61839564c69e3f1ea306525ff160d43fd
Sami Sabri <46608210+sabrisam@users.noreply.github.com>
2026-05-25 19:49:31 -0400

    auto: next.config.mjs @ 19:49:31

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.supabase.co",
+        pathname: "/storage/v1/object/public/**",
+      },
+      {
+        protocol: "https",
+        hostname: "*.digitaloceanspaces.com",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
